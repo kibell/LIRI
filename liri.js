f1438c75db0382887dc2f70f3a54a6d8ba37c68f
Kitrick Bell <kitrickd.bell@gmail.com>
2019-11-24 09:55:59 -0600

    added dot env file and console logged to ensure node recognizes the external file

--- liri.js
+++ liri.js
@@ -1,4 +1,14 @@
 require("dotenv").config();
-const keys = require("keys.js");
+// const keys = require("keys.js");
 
-const spotify = new Spotify(keys.spotify)
+// const spotify = new Spotify(keys.spotify)
+
+const {
+    OMDB_API,
+    Spotify_API,
+    BANDS_IN_TOWN 
+} = process.env;
+
+console.log(OMDB_API)
+console.log(Spotify_API)
+console.log(BANDS_IN_TOWN)
