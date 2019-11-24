310e64b067450050bfc3f5804eeb3e868a5f814b
Kitrick Bell <kitrickd.bell@gmail.com>
2019-11-24 15:40:17 -0600

    modifying console logs to capture the information needed for each API

--- liri.js
+++ liri.js
@@ -16,8 +16,8 @@ const {
 // console.log(Spotify_API)
 // console.log(BANDS_IN_TOWN)
 
-const action = process.argv[2];
-const event = process.argv[3]
+let action = process.argv[2];
+let event = process.argv[3]
 const movieUrl = `http://www.omdbapi.com/?t=${event}&y=&plot=short&apikey=trilogy`;
 const concertUrl = `https://rest.bandsintown.com/artists/${event}/events?app_id=codingbootcamp`  
 const spotUrl = "https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx"
@@ -26,17 +26,17 @@ let spotify = new Spotify(keys.spotify)
 
 switch(action){
     case "concert-this":
-            console.log(BANDS_IN_TOWN);
+            
             concert();
     break;
 
     case "spotify-this-song":
-         console.log(Spotify_API);
+         
          spot();
         break;
 
     case"movie-this":
-    console.log(OMDB_API);
+   
     movie();
     break;
     
@@ -50,7 +50,14 @@ function movie() {
 
 axios.get(movieUrl).then(
     function(response) {
-      console.log(response.data);
+      console.log("Title: " + response.data.Title);
+      console.log("Year: " + response.data.Year);
+      console.log("The IMDB Rating for this Movie is : " + response.data.imdbRating);
+      console.log("The Rotten Tomatoes Rating for this Movie is : " + response.data.Ratings[1].Value)
+      console.log("This film was produced in  " + response.data.Country);
+      console.log("Language:  " + response.data.Language);
+      console.log("Plot:  " + response.data.Plot);
+      console.log("Actors:  " + response.data.Actors);
     })
     .catch(function(error) {
       if (error.response) {
@@ -80,7 +87,7 @@ axios.get(movieUrl).then(
 function concert(){
     axios.get(concertUrl).then(
         function(response) {
-          console.log(response.data);
+          console.log(response.data[0].artist.name);
         })
         .catch(function(error) {
           if (error.response) {
@@ -110,11 +117,28 @@ function concert(){
 
 function spot(){
 
+//not working ask questions
+    if(event == null){
+        event = "humble";
+   };
+
+    
+
+
     spotify.search({ type: "track", query: event }, function(err, result) {
+       
+
         if (err) {
             console.log(err);
-        }
- console.log(result.tracks.items[0].artists[0].name)
+        };
+
+       
+        // console.log(result.tracks.items
+ console.log("Artist: " + result.tracks.items[0].artists[0].name)
+ console.log("Song Title: " + result.tracks.items[0].name)
+ console.log("Preview This Track: " + result.tracks.items[0].preview_url)
+ console.log("Album: " + result.tracks.items[0].album.name)
+ 
 
 });
 
