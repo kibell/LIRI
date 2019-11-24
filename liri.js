4352c80eb8c820658fe797100187f2eaac27851d
Kitrick Bell <kitrickd.bell@gmail.com>
2019-11-24 14:03:14 -0600

    adding spotify keys  and got a response from the API

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const axios = require("axios")
-const inquirer = require("inquirer")
-// const keys = require("keys.js");
+// const inquirer = require("inquirer")
+const Spotify = require('node-spotify-api');
+const keys = require("./key");
 
 // const spotify = new Spotify(keys.spotify)
 
@@ -17,8 +18,11 @@ const {
 
 const action = process.argv[2];
 const event = process.argv[3]
-const queryUrl = `http://www.omdbapi.com/?apikey=trilogy&s=${event}`;
-console.log(queryUrl)
+const movieUrl = `http://www.omdbapi.com/?t=${event}&y=&plot=short&apikey=trilogy`;
+const concertUrl = `https://rest.bandsintown.com/artists/${event}/events?app_id=codingbootcamp`  
+const spotUrl = "https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx"
+let spotify = new Spotify(keys.spotify)
+
 
 switch(action){
     case "concert-this":
@@ -28,7 +32,7 @@ switch(action){
 
     case "spotify-this-song":
          console.log(Spotify_API);
-         Spotify();
+         spot();
         break;
 
     case"movie-this":
@@ -43,9 +47,8 @@ switch(action){
 
 function movie() {
 
-console.log("lol")
 
-axios.get(queryUrl).then(
+axios.get(movieUrl).then(
     function(response) {
       console.log(response.data);
     })
@@ -70,11 +73,49 @@ axios.get(queryUrl).then(
       console.log(error.config);
    
 
-});
+    });
 };
 
 
+function concert(){
+    axios.get(concertUrl).then(
+        function(response) {
+          console.log(response.data);
+        })
+        .catch(function(error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+       
+    
+        });
 
 
+}
 
-function concert(){}
+function spot(){
+
+    spotify.search({ type: "track", query: event }, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+ console.log(result.tracks.items[0].artists[0].name)
+
+});
+
+}
